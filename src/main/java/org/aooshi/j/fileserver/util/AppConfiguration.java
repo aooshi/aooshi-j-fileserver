78a4b07b8a259fb3a7ea9288507db9715fadd621
xiaobogit <lxb@aooshi.org>
2019-08-07 13:14:18 +0800

    config rebuild

--- src/main/java/org/aooshi/j/fileserver/util/AppConfiguration.java
+++ src/main/java/org/aooshi/j/fileserver/util/AppConfiguration.java
@@ -3,17 +3,23 @@ package org.aooshi.j.fileserver.util;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 public class AppConfiguration {
-	
+
 	public static final AppConfiguration singleton = new AppConfiguration();
-	
+
+	private final Logger logger = LogManager.getLogger(AppConfiguration.class);
+
 	private String limitMaxRequestSize;
 	private String limitMaxFileSize;
 	private String basePath;
 	private String userHandler;
-		
+	private String tokenHandler;
+	private int tokenExpire;
+
 	public String getLimitMaxRequestSize() {
 		return limitMaxRequestSize;
 	}
@@ -30,25 +36,44 @@ public class AppConfiguration {
 		return userHandler;
 	}
 
-	private AppConfiguration()
-	{	
-		 Properties props = null;
-		 
-		 try {
+	public String getTokenHandler() {
+		return tokenHandler;
+	}
+
+	public int getTokenExpire() {
+		return tokenExpire;
+	}
+
+	private AppConfiguration() {
+		Properties props = null;
+
+		try {
 			props = PropertiesLoaderUtils.loadAllProperties("application.properties");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		 
-		 if (props != null)
-		 {
+
+		if (props != null) {
 			// fileserver.limit.maxrequestsize =10M
 			// fileserver.limit.maxfilesize =50M
-			 
-			this.limitMaxFileSize  =  props.getProperty("fileserver.limit.maxfilesize");
-			this.limitMaxRequestSize =  props.getProperty("fileserver.limit.maxrequestsize");
+
+			this.limitMaxFileSize = props.getProperty("fileserver.limit.maxfilesize");
+			this.limitMaxRequestSize = props.getProperty("fileserver.limit.maxrequestsize");
 			this.basePath = props.getProperty("basePath");
 			this.userHandler = props.getProperty("fileserver.user.handler");
-		 }
+
+			//
+			this.tokenHandler = props.getProperty("fileserver.token.handler");
+			this.tokenExpire = Integer.parseInt(props.getProperty("fileserver.token.expire"));
+			// =86400
+
+			//
+			this.logger.info("fileserver.limit.maxfilesize: " + this.limitMaxFileSize);
+			this.logger.info("fileserver.limit.maxrequestsize: " + this.limitMaxRequestSize);
+			this.logger.info("fileserver.limit.basePath: " + this.basePath);
+			this.logger.info("fileserver.limit.fileserver.user.handler: " + this.userHandler);
+			this.logger.info("fileserver.token.handler: " + this.tokenHandler);
+			this.logger.info("fileserver.token.expire: " + this.tokenExpire);
+		}
 	}
 }
